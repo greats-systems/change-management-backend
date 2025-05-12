1729c30cc0c70c81319fdcf1a522b8fdefefcccc
Ngqabutho-Moyo <your.moyongqaa@gmail.com>
2025-05-12 13:39:45 +0200

    Removed test.js and vercel.json

--- test.js
+++
@@ -1,6 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-const env = require("dotenv");
-require("express");
-env.config();
-const url = process.env.URL
-console.log(`URL: ${url}`)
